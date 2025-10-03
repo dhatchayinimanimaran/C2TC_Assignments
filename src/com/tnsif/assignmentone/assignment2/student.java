7781bd11fa7e5031b60b9455f0066a2953f8108e
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-03 20:51:50 +0530

    commit

---
+++ src/com/tnsif/assignmentone/assignment2/student.java
@@ -0,0 +1,13 @@
+package com.tnsif.assignmentone.assignment2;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // Default constructor
+	    public Student() {
+	        System.out.println("Student object is created");
+
+	}
+
+}
