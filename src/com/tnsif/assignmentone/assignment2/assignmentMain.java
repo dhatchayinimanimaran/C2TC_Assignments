bdf1f48ad6c0705dd23e7154acf3a5e6aedc2f84
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-03 20:48:38 +0530

    commit

---
+++ src/com/tnsif/assignmentone/assignment2/assignmentMain.java
@@ -0,0 +1,20 @@
+package com.tnsif.assignmentone.assignment2;
+
+public class assignmentMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+	        // Create Student object (will print message)
+	        Student student = new Student();
+
+	        // Create Commission object
+	        Commission emp = new Commission();
+
+	        // Accept details and calculate commission
+	        emp.acceptDetails();
+	        emp.calculateCommission();
+
+	}
+
+}
