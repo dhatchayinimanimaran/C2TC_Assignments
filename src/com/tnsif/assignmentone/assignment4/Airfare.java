b797a639da316ffac5af9b156362b86711c3c520
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 10:42:29 +0530

    commit

---
+++ src/com/tnsif/assignmentone/assignment4/Airfare.java
@@ -0,0 +1,12 @@
+package com.tnsif.assignmentone.assignment4;
+
+public class Airfare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Double calculateAmount(); // abstract method
+	    void display();            // method to display details
+
+	}
+
+}
