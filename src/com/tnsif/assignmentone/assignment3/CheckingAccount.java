b9e8edbb0865f29e7ea242bf0b5d4913a1b874b7
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 10:35:42 +0530

    commit

---
+++ src/com/tnsif/assignmentone/assignment3/CheckingAccount.java
@@ -0,0 +1,35 @@
+package com.tnsif.assignmentone.assignment3;
+
+public class CheckingAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private double overdraftLimit = 5000;
+
+	    public CheckingAccount(String name, double balance) {
+	        super(name, balance);
+	    }
+
+	    @Override
+	    public void deposit(double amount) {
+	        balance += amount;
+	        System.out.println("Deposited ₹" + amount + ". New Balance: ₹" + balance);
+	    }
+
+	    @Override
+	    public void withdraw(double amount) {
+	        if (balance + overdraftLimit >= amount) {
+	            balance -= amount;
+	            System.out.println("Withdrew ₹" + amount + ". New Balance: ₹" + balance);
+	        } else {
+	            System.out.println("Overdraft limit exceeded!");
+	        }
+	    }
+
+	    @Override
+	    public double getBalance() {
+	        return balance;
+
+	}
+
+}
