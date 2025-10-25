92fba729d3ac0ae1c06b10bbeeaafbaa08551db7
LAPCARE TECH <LAPCARE TECH@DESKTOP-38JJU09>
2025-10-25 10:34:10 +0530

    commit

---
+++ src/com/tnsif/assignmentone/assignment3/Bank.java
@@ -0,0 +1,18 @@
+package com.tnsif.assignmentone.assignment3;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private static int totalAccounts = 0;
+
+	    public static void incrementAccountCount() {
+	        totalAccounts++;
+	    }
+
+	    public static int getTotalAccounts() {
+	        return totalAccounts;
+
+	}
+
+}
